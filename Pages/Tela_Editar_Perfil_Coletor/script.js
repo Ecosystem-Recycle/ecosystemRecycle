b30a5601e1968d886c9faf50dba45724934b38af
Carlos Augusto Da Silva Santos <code.carlosaugusto@gmail.com>
2023-07-03 20:30:36 -0300

    finalização da página tela

---
+++ Pages/Tela_Editar_Perfil_Coletor/script.js
@@ -0,0 +1,94 @@
+//Criano váriavel em JS
+let menu = document.getElementById("menu_links")
+let menu_barras = document.getElementById("menu_barras")
+let menu1 = document.getElementById("menu_login")
+let menu2 = document.getElementById("menu_login2")
+let menuNot1 = document.getElementById("menu_notifique1")
+let menuNot2 = document.getElementById("menu_notifique2")
+
+function mostrarMenu(){
+    if (window.getComputedStyle(menu).display == 'none'){
+        //Mostre o menu
+        menu.style.display = "flex";
+        menu_barras.setAttribute("aria-expanded", "true");
+        menu_barras.setAttribute("aria-label", "Fechar o Menu");
+
+    }else{
+        //Esconde o menu
+        menu.style.display = "none";
+
+        menu_barras.setAttribute("aria-expanded", "false");
+        menu_barras.setAttribute("aria-label", "Abrir o Menu");
+        
+    }
+};
+
+addEventListener("resize", () => {
+    if (window.innerWidth > 1073){
+        menu.style.display = "flex";
+    } else{
+        menu.style.display = "none";
+    }
+});
+
+
+
+function abrirFechar(){
+    if (window.getComputedStyle(menu1).display == 'none'){
+        //Mostre o menu
+        menu1.style.display = "flex";
+
+
+    }else{
+        //Esconde o menu
+        menu1.style.display = "none";
+
+        // menu_barras.setAttribute("aria-expanded", "false");
+        // menu_barras.setAttribute("aria-label", "Abrir o Menu");
+        
+    }
+};
+
+function abrirFechar2(){
+    if (window.getComputedStyle(menu2).display == 'none'){
+        //Mostre o menu
+        menu2.style.display = "flex";
+
+
+    }else{
+        //Esconde o menu
+        menu2.style.display = "none";
+
+        // menu_barras.setAttribute("aria-expanded", "false");
+        // menu_barras.setAttribute("aria-label", "Abrir o Menu");
+        
+    }
+};
+
+function abrirFecharNote2(){
+    if (window.getComputedStyle(menuNot2).display == 'none'){
+        //Mostre o menu
+        menuNot2.style.display = "flex";
+
+
+    }else{
+        //Esconde o menu
+        menuNot2.style.display = "none";
+        
+    }
+};
+
+function abrirFecharNote1(){
+    if (window.getComputedStyle(menuNot1).display == 'none'){
+        //Mostre o menu
+        menuNot1.style.display = "flex";
+
+
+    }else{
+        //Esconde o menu
+        menuNot1.style.display = "none";
+        
+    }
+};
+
+
